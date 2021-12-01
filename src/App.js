a5620662b09e6584564c2f9555257fae8df044ce
wazimuhb <hesbonosoro1@gmail.com>
2021-12-01 18:23:28 +0300

    Pusher config in React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Pusher from 'pusher-js';
 import './App.css';
 import Chat from './components/Chat';
 import Sidebar from './components/Sidebar';
 
 function App() {
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		const pusher = new Pusher('2bda85460a9ec3e2b216', {
+			cluster: 'mt1',
+		});
+
+		const channel = pusher.subscribe('messages');
+		channel.bind('inserted', data => {
+			setMessages([...messages, data]);
+		});
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]);
 	return (
 		<div className="app">
 			<div className="app__body">
