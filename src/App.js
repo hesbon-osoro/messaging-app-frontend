3a6c805b62c167b1a1a01cb01d4d5c6ef1813660
wazimuhb <hesbonosoro1@gmail.com>
2021-12-01 03:54:35 +0300

    Sidebar component reusing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
-	return <div className="App">Messaging App</div>;
+	return (
+		<div className="app">
+			<div className="app__body">
+				<Sidebar />
+			</div>
+		</div>
+	);
 }
 
 export default App;
