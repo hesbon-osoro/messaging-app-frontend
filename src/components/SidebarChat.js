423451736f1e74715f39ceed6fb2b02d2fe1478f
wazimuhb <hesbonosoro1@gmail.com>
2021-12-02 03:21:18 +0300

    refactoring the hardcode and making a reusable component

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -2,7 +2,7 @@ import { Avatar } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 
-const SidebarChat = () => {
+const SidebarChat = ({ messages }) => {
 	const [seed, setSeed] = useState('');
 	useEffect(() => {
 		setSeed(Math.floor(Math.random() * 5000));
@@ -11,8 +11,8 @@ const SidebarChat = () => {
 		<div className="sidebarChat">
 			<Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
 			<div className="sidebarChat__info">
-				<h2>Room name</h2>
-				<h2>Last message...</h2>
+				<h2>Wazimu Dev Chat Corner</h2>
+				<h2>{messages[messages.length - 1]?.message}</h2>
 			</div>
 		</div>
 	);
